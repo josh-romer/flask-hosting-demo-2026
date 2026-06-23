9b79a02efdacaf99641f4aa67e81c0194e6102a9
Josh Romer <jromer94@gmail.com>
2026-06-23 16:06:12 -0400

    Disable FlaskBehindProxy usage
    Comment out FlaskBehindProxy initialization.

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm
-from flask_behind_proxy import FlaskBehindProxy
+# from flask_behind_proxy import FlaskBehindProxy
 
 app = Flask(__name__)
-proxied = FlaskBehindProxy(app)
+# proxied = FlaskBehindProxy(app)
 app.config['SECRET_KEY'] = 'c29bcfa698752666def85f68880d22d8'
 
 @app.route("/")
