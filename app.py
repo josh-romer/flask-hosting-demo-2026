cfe2e3a5a796d6c0e0627f2c5d311157f0559e37
josh-romer <jromer94@gmail.com>
2026-06-23 17:00:30 -0400

    added email validator import

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 import git
+import email_validator
 from flask_sqlalchemy import SQLAlchemy
 
 # from flask_behind_proxy import FlaskBehindProxy
